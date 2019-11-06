7dd14227f0e739eb70bef05764e8edfd4fda778e
Madeline Corman <madelinecorman@gmail.com>
2019-11-06 16:11:41 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -47,7 +47,7 @@ end
 def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
-  
+  local_copy_of_matrix = assembled_matrix.dup
   assembled_matrix[0][2] = "Tiny Ewell"
   return assembled_matrix
   
