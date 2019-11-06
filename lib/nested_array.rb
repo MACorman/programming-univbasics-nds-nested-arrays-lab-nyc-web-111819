3561a8931216fa35bf28d5538a8c37074d0d7605
Madeline Corman <madelinecorman@gmail.com>
2019-11-06 15:52:17 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -49,5 +49,4 @@ def matrix_update(matrix, row, column, new_value)
   # Return the updated matrix
   new_value = "Tiny Green"
   matrix_update = assembled_matrix[0][2][new_value]
-  return assembled_matrix
 end
