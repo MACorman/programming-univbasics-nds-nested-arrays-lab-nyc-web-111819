16c87b4a79b87512b1c9c90ad762d6d9ef1596f3
Madeline Corman <madelinecorman@gmail.com>
2019-11-06 15:39:03 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -32,9 +32,11 @@ def array_literal_matrix
   # alphabetically by the first character.
   
   array_literal_matrix = [
-    ["Don Gately", "Joelle van Dyne", "Pat M.", "Kate Gompert", "Bruce Green"].sorted,
-    ["Hal Incandenza", "Lyle", "Gerhard Schtitt", "Mario Incandenza", "Michael Pemulis"].sorted
+    ["Don Gately", "Joelle van Dyne", "Pat M.", "Kate Gompert", "Bruce Green"],
+    ["Hal Incandenza", "Lyle", "Gerhard Schtitt", "Mario Incandenza", "Michael Pemulis"]
     ]
+    
+    array_literal_matrix = 
 end
 
 def matrix_lookup(matrix, row, column)
