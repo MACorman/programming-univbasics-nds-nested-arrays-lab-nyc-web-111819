3a4f4d95aa68fa698a3de614e725c3d84919018d
Madeline Corman <madelinecorman@gmail.com>
2019-11-06 15:53:45 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -48,6 +48,6 @@ def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
   new_value = "Tiny Green"
-  matrix_update = assembled_matrix[0][2][new_value]
+  matrix_update = assembled_matrix[0][2]new_value
   return matrix_update
 end
